abc700ea07b34d61e60727ac38874eacedee398f
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-18 17:55:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 
 
 def fizzbuzz(int) 
-  if int % 3 == 0
+  if int % 5 == 0 && int % 3 == 0
+    "Fizzbuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
-  elsif int % 5 == 0 && int % 3 == 0
-    "Fizzbuzz"
   else 
     nil
   end 
